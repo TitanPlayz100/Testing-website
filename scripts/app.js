eee465c8b895d965c5540d6f723d14c9c4da4cfb
TitanPlayz <118145727+TitanPlayz100@users.noreply.github.com>
2023-12-12 14:37:50 +1100

    added some responsive things

--- scripts/app.js
+++ scripts/app.js
@@ -21,9 +21,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementsByClassName("navbar")[0].style.padding = "0% 10%";
+    document.getElementsByClassName("navbar")[0].style.padding = "0vw 10vw";
   } else {
-    document.getElementsByClassName("navbar")[0].style.padding = "2% 20%";
+    document.getElementsByClassName("navbar")[0].style.padding = "2vw 20vw";
   }
 }
 
@@ -32,12 +32,12 @@ var navbarItems = `
 <nav class="navbar">
   <ul>
     <li><img src="assets/logo.png" class="navimg"></li>
-    <li><strong><a style="font-size: xx-large; color: white;" href="https://titanplayz100.github.io/">TitanPlayz's
+    <li><strong><a class="navtext" href="https://titanplayz100.github.io/">TitanPlayz's
           Website</a></strong></li>
   </ul>
   <ul>
     <li><a href="https://youtube.com/@titanplayz100" role="button" class="contrast outline">Youtube</a>
-    <li><a href="about-me.html" role="button" class=" contrast outline">About Me</a>
+    <li><a href="about-me.html" role="button" class="contrast outline">About Me</a>
     <li><a href="index.html" role="button" class="contrast">Home Page</a>
   </ul>
 </nav>
