b4e10f7e247ec66fe6c814e19aebc4c00ac73149
TitanPlayz <118145727+TitanPlayz100@users.noreply.github.com>
2023-12-13 22:33:16 +1100

    finnaly kinda responsive

--- scripts/app.js
+++ scripts/app.js
@@ -19,12 +19,24 @@ hiddenElements.forEach((el) => {
 // minimises top navbar on scroll
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementsByClassName("navbar")[0].style.padding = "0vw 10vw";
+  if (window.innerWidth > 1000) {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+      document.getElementsByClassName("navbar")[0].style.padding = "0vw 10vw";
+      document.getElementsByClassName("navtext")[0].style.fontSize = "1.5vw";
+      document.getElementsByClassName("navimg")[0].style.width = "3vw";
+    } else {
+      document.getElementsByClassName("navbar")[0].style.padding = "2vw 20vw";
+      document.getElementsByClassName("navtext")[0].style.fontSize = "2.5vw";
+      document.getElementsByClassName("navimg")[0].style.width = "4vw";
+    }
   } else {
-    document.getElementsByClassName("navbar")[0].style.padding = "2vw 20vw";
+      document.getElementsByClassName("navbar")[0].style.padding = "";
+      document.getElementsByClassName("navtext")[0].style.fontSize = "";
+      document.getElementsByClassName("navimg")[0].style.width = "";
   }
+  
 }
 
 // navbar
@@ -35,7 +47,7 @@ var navbarItems = `
     <li><strong><a class="navtext" href="https://titanplayz100.github.io/">TitanPlayz's
           Website</a></strong></li>
   </ul>
-  <ul>
+  <ul class="hide_on_mobile">
     <li><a href="https://youtube.com/@titanplayz100" role="button" class="contrast outline">Youtube</a>
     <li><a href="about-me.html" role="button" class="contrast outline">About Me</a>
     <li><a href="index.html" role="button" class="contrast">Home Page</a>
@@ -45,7 +57,6 @@ var navbarItems = `
 document.getElementsByClassName("container")[0].insertAdjacentHTML("beforebegin", navbarItems);
 
 // footer
-
 var footerItems = `
 <div class="footer show">
   <center>
