f2c02ca2c761ac8ef3e5ca05655b1e31a63f56c1
TitanPlayz <118145727+TitanPlayz100@users.noreply.github.com>
2024-07-19 15:36:15 +1000

    fixed stuff

--- channel_list/parsedata.py
+++ channel_list/parsedata.py
@@ -3,12 +3,10 @@ Import necessary pip packages,
 Get oauth client and secret using google cloud console with youtube API, 
 Run python parsedata.py
 '''
-
-import json
-import os
-
 # pip install --upgrade google-api-python-client
 # pip install --upgrade google-auth-oauthlib google-auth-httplib2
+from json import dump
+from os import environ
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
@@ -16,72 +14,49 @@ import googleapiclient.errors
 entire_sub_list = []
 
 def get_subs():
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
     api_service_name = "youtube"
     api_version = "v3"
-
-    # you have to make oauth client by going to google clound console
-    client_secrets_file = "client_secrets.json"  
+    client_secrets_file = "client_secrets.json"
 
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
     credentials = flow.run_local_server()
     youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
-    
     process_request(youtube)
-
 
 def process_request(youtube, next_page_token=None):
     request = None
-    if next_page_token is not None:
-        request = youtube.subscriptions().list(
-            part="snippet",
-            channelId="UCUCtxzo3wXprH383RMQKlYQ",
-            maxResults=50,
-            order="alphabetical",
-            prettyPrint=True,
-            pageToken=next_page_token
-        )
-    else:
-        request = youtube.subscriptions().list(
-            part="snippet",
-            channelId="UCUCtxzo3wXprH383RMQKlYQ",
-            maxResults=50,
-            order="alphabetical",
-            prettyPrint=True
-        )
-
+    request = youtube.subscriptions().list(
+        part="snippet",
+        channelId="UCUCtxzo3wXprH383RMQKlYQ",
+        maxResults=50,
+        order="alphabetical",
+        prettyPrint=True,
+        pageToken=next_page_token
+    )
+    
     response: dict = request.execute()
     entire_sub_list.append(response)
     next_page_token = response.get("nextPageToken", None)
-
     if next_page_token is not None:
         process_request(youtube, next_page_token)
     
-
 def parse_subs():
-    subs = entire_sub_list
-
     subslist = []
-    for page in subs:
+    for page in entire_sub_list:
         for channel in page["items"]:
             info = channel["snippet"]
-
             id = info["resourceId"]["channelId"]
-            name = info["title"]
-            desc = info["description"]
-            pfp = info["thumbnails"]["default"]["url"]
             subslist.append({
-                "name": name,
-                "desc": desc,
-                "pfp": pfp,
+                "name": info["title"],
+                "desc": info["description"],
+                "pfp": info["thumbnails"]["default"]["url"],
                 "link": f'https://www.youtube.com/channel/{id}'
             })
 
     with open('subscriptions.json', 'w') as out:
-        json.dump(subslist, out)
-
+        dump(subslist, out)
 
 if __name__ == "__main__":
     get_subs()
--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,31 @@
-// adds blur to scrolling
+// blur to scrolling
 const observer = new IntersectionObserver(entries =>
   entries.forEach(entry =>
     entry.isIntersecting
-      ? entry.target.classList.add('visible')
-      : entry.target.classList.remove('visible')
+      ? entry.target.classList.add('show')
+      : entry.target.classList.remove('show')
   )
 );
 
-document.getElementsByTagName('div').forEach(el => observer.observe(el))
+[...document.getElementsByTagName('div')].forEach(el => observer.observe(el))
 
 // smooth scrolling
-function scrollToTop() { window.scrollTo({ top: 0, behavior: 'smooth' }) }
+function scrollToTop() { scrollTo({ top: 0, behavior: 'smooth' }) }
 
 // minimises top navbar on scroll
-window.onscroll = () => {
-  const cond1 = innerWidth > 1000, cond2 = document.body.scrollTop > 90
+onscroll = () => {
+  const cond1 = innerWidth > 1000, cond2 = document.documentElement.scrollTop > 90
   document.getElementById("navbar").style.padding = cond1 ? cond2 ? "0vw 10vw" : "2vw 20vw" : "";
   document.getElementById("navtext").style.fontSize = cond1 ? cond2 ? "1.5vw" : "2.5vw" : "";
   document.getElementById("navimg").style.width = cond1 ? cond2 ? "3vw" : "4vw" : "";
 }
 
 // navbar
-var navbarItems = `
-<nav class="navbar" id="navbar">
+const navbarItems = `
+<nav id="navbar">
   <ul>
-    <li><img src="assets/logo.png" class="navimg" id="navimg"></li>
-    <li><strong><a class="navtext" href="https://titanplayz100.github.io/">TitanPlayz's
-          Website</a></strong></li>
+    <li><img src="assets/logo.png" id="navimg"></li>
+    <li><a id="navtext" href="https://titanplayz100.github.io/">TitanPlayz's Website</a></li>
   </ul>
   <ul class="hide_on_mobile">
     <li><a href="https://youtube.com/@titanplayz100" role="button" class="contrast outline">Youtube</a>
@@ -35,14 +34,13 @@ var navbarItems = `
   </ul>
 </nav>
 `
-document.getElementsByTagName("main").insertAdjacentHTML("beforebegin", navbarItems);
+document.getElementsByTagName("main")[0].insertAdjacentHTML("beforebegin", navbarItems);
 
 // footer
-var footerItems = `
-<div class="footer show" id="footer">
-  <center>
+const footerItems = `
+<div class="show" id="footer">
     <a href="" role="button" class="secondary outline" onclick="scrollToTop();return false">Back to top</a>
-  </center>
+    <a class="secondary" style="margin: 1vw;" href="https://github.com/TitanPlayz100/titanplayz100.github.io">Github Repo</a>
 </div>
 `
-document.getElementsByTagName("main").insertAdjacentHTML("afterend", footerItems);
+document.getElementsByTagName("main")[0].insertAdjacentHTML("afterend", footerItems);
